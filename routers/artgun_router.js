d82e583f656dd7b0c8907c540a955812357cba64
Andrew Carton <andrew.carton1@gmail.com>
2016-12-09 01:44:11 -0500

    pdf moves

--- routers/artgun_router.js
+++ routers/artgun_router.js
@@ -217,7 +217,7 @@ artGunRouter.get('/orders/shipment', function(req, res) {
   pdf.create(html, options).toFile('./routers/packing_slips/packSlipTest3.pdf', function(err, poop) {
     if (err) return console.log(err);
     console.log(poop);
-    res.sendFile(poop);
+    res.download(poop.filename);
   });
 });
 
