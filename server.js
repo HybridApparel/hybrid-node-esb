6fae04cadf79416456581032a24049f1cd759cb7
Andrew Carton <andrew.carton1@gmail.com>
2016-12-09 02:10:00 -0500

    pdf moves

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ app.use( express.static( path.join( application_root, 'browser' )));
 
 app.use('/artgun', artGunRouter);
 
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
 
 module.exports = app;
 
