099164010d4d73aa557b2174b8a467be9b3842af
Andrew Carton <andrew.carton1@gmail.com>
2016-12-15 14:17:35 -0500

    cleanup

--- routers/artgun_router.js
+++ routers/artgun_router.js
@@ -218,7 +218,7 @@ artGunRouter.get('/orders/shipment', function(req, res) {
   var html = fs.readFileSync('./public/packSlipTest.html', 'utf8');
   var options = {
     "type": "pdf",
-    "base": __dirname
+    "base": 'http://tranquil-fortress-90513.herokuapp.com/'
     };
   
   pdf.create(html, options).toFile('./packing_slips/packing_slips/packSlipTest8.pdf', function(err, file) {
