503679bed9e0f2e685188c2a9608d465d8b89a84
Andrew Carton <andrew.carton1@gmail.com>
2016-12-28 14:13:42 -0500

    fixing camel case column name error

--- models/shipments.js
+++ models/shipments.js
@@ -9,7 +9,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        shipments.belongsTo(models.orders, {foreighKey: "order_id"});
+        shipments.belongsTo(models.orders, {foreighKey: "orderID"});
       }
     }
   });
