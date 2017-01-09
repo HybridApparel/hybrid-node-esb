cf7d0f62c5e1ae76efac4ecaadb7961f1b73f473
Andrew Carton <andrew.carton1@gmail.com>
2017-01-09 14:17:24 -0500

    fix pack slip error

--- routers/artgun_router.js
+++ routers/artgun_router.js
@@ -257,7 +257,7 @@ artGunRouter.get('/orders/:orderID/packslip', function(req, res) {
       pdf.create(html, options).toFile(fileNameWrite, function(err, file) {
         if (err) return console.log(err);
         console.log(file);
-        res.status(200).download(file.filename).send('Your download will begin shortly');
+        res.download(file.filename)status(200);
       });
     });
   console.log('heres the end of the pack slip route');
