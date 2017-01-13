aa75964d546c82a4c5c4ddb379c88dee5ca09d2c
Andrew Carton <andrew.carton1@gmail.com>
2017-01-12 20:58:07 -0500

    fixing jpg pack slip

--- routers/tsc_router.js
+++ routers/tsc_router.js
@@ -279,7 +279,6 @@ TSCRouter.get('/orders/:orderID/packslip', function(req, res) {
         "type": "jpeg",
         "base": 'http://tranquil-fortress-90513.herokuapp.com/',
         "quality": "100",
-        "zoomFactor": "2",
         "orientation": "portrait"
       };
       var fileNameWrite = 'packSlip_' + orderXID + '.jpg';
