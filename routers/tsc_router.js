79b77c2f9afb9a9d893e45ab8a9263c3a099fab8
Andrew Carton <andrew.carton1@gmail.com>
2017-01-13 14:40:22 -0500

    fixing jpg pack slip

--- routers/tsc_router.js
+++ routers/tsc_router.js
@@ -277,7 +277,7 @@ TSCRouter.get('/orders/:orderID/packslip', function(req, res) {
 
       var html = compPackSlipHTML(templateSourceJSON);
       var fileNameWrite = 'packSlip_' + orderXID + '.jpg';
-      wkhtmltoimage.generate(html, {output: 'outTest.jpg'}, function () {
+      wkhtmltoimage.generate(html, {output: '/app/outTest.jpg'}, function () {
         console.log('outTest.jpg');
         res.download('outTest.jpg');
       });
