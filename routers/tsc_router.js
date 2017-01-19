954fe0df3cb7b6c69080757ca172748843153062
Andrew Carton <andrew.carton1@gmail.com>
2017-01-19 13:37:11 -0500

    more tsc route testing

--- routers/tsc_router.js
+++ routers/tsc_router.js
@@ -235,8 +235,8 @@ var persistTSCShipment = function (shipmentJSON) {
 TSCRouter.get('/orders/:xid/teststatus', function(req, res) {
   console.log('new check status route hit');
   console.log(JSON.stringify(req.headers));
-  console.log('with the app get is ' + req.get(path));
-  console.log('with the app get is ' + req.get(route));
+  console.log('with the app get is ' + req.get('path'));
+  console.log('with the app get is ' + req.get('route'));
   Order.findOne({
     where: {OrderID: req.params.xid}
   }).then(function(order) {
